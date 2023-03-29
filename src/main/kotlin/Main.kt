9212ec9dc24083735add4093e752306f8b6d3ea5
margaret-ngugi <ngugiwairimumargaret@gmail.com>
2023-03-29 23:24:49 +0300

    changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main() {
     //Q1.
     println()
-    println()
+    println(vehicleObject(listOf()))
 
 
 }
@@ -15,21 +15,22 @@ fun createNamesList(names:String):Int{
 //Q2.
 fun peopleHeight(names: String,height:Int):Int{
     val people = listOf("Mary",40.0,"John",30.5,"Agnes",35.0,"Faith",42.0)
+
     return height
 
 }
 //Q3.
 data class person(var names: String,var  age:Int,var height: Int,var  weight:Int)
-fun personObjects = listOf{
-    val person1 = person("Agnes",23,2,40)
-    val person2 = person("Linda",24,3,65)
-    val person3 = person("John",25,2,55)
-    val personList = listOf(person1,person2,person3)
-    val sorted = personList.sortedBy{person ->person.age}
+fun personObjects(){
+    val person1 = person("Agnes", 23, 2, 40)
+    val person2 = person("Linda", 24, 3, 65)
+    val person3 = person("John", 25, 2, 55)
+    val personList = listOf(person1, person2, person3)
+    val sorted = personList.sortedByDescending { person -> person.age }
 
-    }
+}
 //4.
-fun people = listOf{
+fun people (){
 val person1 = person("Agnes",23,2,40)
 val person2 = person("Linda",24,3,65)
 val person3 = person("John",25,2,55)
@@ -41,11 +42,22 @@ val personList = listOf(person1,person2,person3)
 
 //Q5.
 data class vehicle(var registration:String,var mileage:Int)
-fun carObject = listOf{
+fun vehicleObject(vehicle: List<vehicle>):Int{
 var x = vehicle("KWSCF",42)
     var w = vehicle("KWCFT",60)
     var z = vehicle("Kwerd",80)
-}
+    var allVehicles = listOf(x,w,z)
+    var avg=0
+    vehicle.forEach { car ->
+        car.mileage
+        avg += car.mileage
+    }
+        var totalAvg=vehicle.count()
+        return totalAvg
+    }
+
+
+
 
 
 
